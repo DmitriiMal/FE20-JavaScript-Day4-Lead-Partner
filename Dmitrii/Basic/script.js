ffdd52f9cff0f07f8080abedcf058ee7176f8f07
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-03 12:38:54 +0200

    complete basic ex

--- Dmitrii/Basic/script.js
+++ Dmitrii/Basic/script.js
@@ -4,6 +4,7 @@
 
 let submitBtn = document.querySelector('#submit');
 let foolName = document.querySelector('#fool_name');
+// let puserProf = document.querySelector('#profession').value;
 
 
 function displayName(e) {
@@ -19,13 +20,27 @@ function displayName(e) {
     foolName.innerHTML = `Enter your firstname`;
   } else if (lastName.length == 0){
     foolName.innerHTML = `Enter your lastname`;
-  } else if (firstName.length > 5) {
+  } else if (firstName.length + firstName.length > 5) {
       foolName.style.color = 'green';
   } else {
     foolName.style.color = 'red';
   }
+  changeColor()
 }
 
 submitBtn.addEventListener('click', displayName)
 
+// ////////////////////// //
+// ///// Basic Ex 2 ///// //
+// ////////////////////// //
 
+function changeColor() {
+  let puserProf = document.querySelector('#profession').value;
+  if (puserProf == 'it') {
+    foolName.style.backgroundColor = 'purple';
+  } else if (puserProf == 'hospitality') {
+    foolName.style.backgroundColor = 'yellow';
+  } else if (puserProf == 'gastronomy') {
+    foolName.style.backgroundColor = 'blue';
+  } 
+}
